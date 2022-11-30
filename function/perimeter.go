c4895935d5268c88f318f37bf4c3ee5ce35dd9da
aLittlecrocodile <95510183+aLittlecrocodile@users.noreply.github.com>
2022-11-30 21:24:11 +0800

    测试

---
+++ function/perimeter.go
@@ -0,0 +1,33 @@
+package function
+
+import "math"
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.width + rectangle.height)
+
+}
+func Area(rectangle Rectangle) float64 {
+	return rectangle.height * rectangle.width
+
+}
+
+type Rectangle struct {
+	width  float64
+	height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+type Shape interface {
+	Area() float64
+}
---
+++ function/perimeter_test.go
@@ -0,0 +1,33 @@
+package function
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{height: 10.0, width: 10.0}
+	got := Perimeter(rectangle)
+	wanted := 40.0
+	if got != wanted {
+		t.Errorf("wanted %.2f but got %.2f", wanted, got)
+	}
+}
+func TestArea(t *testing.T) {
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("want %g but got %g", want, got)
+		}
+
+	}
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{height: 6.0, width: 7.0}
+		checkArea(t, rectangle, 42.0)
+
+	})
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		checkArea(t, circle, 314.1592653589793)
+
+	})
+
+}
