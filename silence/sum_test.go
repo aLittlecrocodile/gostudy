3e040948f44b8dd5008baef18f9d159fb1bc875f
aLittlecrocodile <95510183+aLittlecrocodile@users.noreply.github.com>
2022-11-30 16:44:34 +0800

    测试

--- silence/sum_test.go
+++ silence/sum_test.go
@@ -3,10 +3,23 @@ package silence
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	wanted := 15
-	if got != wanted {
-		t.Errorf("wanted %d but got %d,%v", wanted, got, numbers)
-	}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		wanted := 15
+		if got != wanted {
+			t.Errorf("wanted %d but got %d,%v", wanted, got, numbers)
+		}
+
+	})
+	t.Run("collection of any numbers ", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		wanted := 6
+		if got != wanted {
+			t.Errorf("wanted %d but got %d,%v", wanted, got, numbers)
+		}
+
+	})
+
 }
